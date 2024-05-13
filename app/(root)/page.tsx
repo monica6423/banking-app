d5f9c1e9d8161486d59585d01a8c344d307bca11
monica6423 <monica6423@gmail.com>
2024-05-13 13:19:21 +0300

    Create Total Balamce Box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user="Guest"
+            subtext="Access and manage your account and transaction efficientlt"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.30}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
