52ca8a9f6da1cca266ca787fe19b2b57391f59ca
monica6423 <monica6423@gmail.com>
2024-05-19 08:41:10 +0300

    Set up createLinkToken and dwolla actions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({ type }:{ type: string }) => {
@@ -51,9 +52,23 @@ const AuthForm = ({ type }:{ type: string }) => {
   const onSubmit = async(data: z.infer<typeof formSchema>) => {
     setisLoading(true);
     try {
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password
+      }
+
       if(type === "sign-up") {
-        const newUser = await signUp(data);
+        const newUser = await signUp(userData);
         setUser(newUser);
+        console.log("sign up")
       }
 
       if(type === "sign-in") {
@@ -64,7 +79,7 @@ const AuthForm = ({ type }:{ type: string }) => {
         if(response) router.push("/")
       }
     } catch (error) {
-      
+      console.log(error)
     } finally {
       setisLoading(false)
     }
@@ -102,7 +117,7 @@ const AuthForm = ({ type }:{ type: string }) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-         
+          <PlaidLink user={user} variant="primary" />
         </div>
       ): (
         <div>
